ee18501ae16701fd1a4d35e0cfe1e104d9d9b81e
Rishi <suryawanshirishi.nmims@gmail.com>
2020-08-01 14:39:18 +0530

    EFAppli10

---
+++ EFAppli10/WebForm1.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EFAppli10
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            EmpDBContext employeeDBContext = new EmpDBContext();
+            GridView1.DataSource = employeeDBContext.Emps;
+            GridView1.DataBind();
+        }
+    }
+}
